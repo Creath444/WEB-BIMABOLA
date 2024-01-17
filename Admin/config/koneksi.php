b43ed501c07de9a71d6cff2e4a3080e2e743eded
Tampan Satria <72635816+Creath444@users.noreply.github.com>
2024-01-17 09:08:42 +0700

    Update koneksi.php

--- Admin/config/koneksi.php
+++ Admin/config/koneksi.php
@@ -1,11 +1,11 @@
 <?php 
+<?php
+$DB_HOST = 'localhost';
+$DB_USER = 'root';
+$DB_PASS = 'kata_sandi';
+$DB_NAME = 'web-bimabola';
 
-$host 		= 'localhost';
-$username 	= 'root';
-$password	= '';
-$dbname		= 'web-bimabola';
-
-$dbhost = mysqli_connect($host,$username,$password,$dbname);
+$dbhost = mysqli_connect($DB_HOST,$DB_USER,$DB_PASS,$DB_NAME);
     if (!$dbhost){
           die("Koneksi gagal:".mysqli_connect_error());
     }
